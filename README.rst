9133d2bc97fdf10bbe40b1694fa4dedd80591187
Mike Yumatov <mike@yumatov.org>
2010-10-11 23:46:46 +0400

    Prepared for first release.

---
+++ README.rst
@@ -0,0 +1,29 @@
+=====
+Snake
+=====
+
+Snake is yet another Make-like tool, written on Python and inspired by Rake.
+
+
+Usage Example
+=============
+
+In a directory with ``snakefile.py`` containing such code::
+
+    from snake.api import *
+
+    @task
+    def hello():
+        print("Hello World!")
+
+    task('default', deps=hello)
+
+one could run::
+
+    $ snake hello
+    Hello World!
+
+or (because ``default`` task is executed if no other task name was specified)::
+
+    $ snake
+    Hello World!
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 
 setup(
     name = 'Snake',
-    version = '0.0.1dev',
+    version = '0.0.1',
+    description = 'Snake is yet another Make-like tool inspired by Rake.',
+    long_description = read('README.rst'),
+    author = 'Mike Yumatov',
+    author_email = 'mike@yumatov.org',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
             'snake = snake.main:main',
         ]
-    }
+    },
 )
