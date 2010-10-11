463e6538af14bdaa40e060a36f8766f5ba0fc6b4
Mike Yumatov <mike@yumatov.org>
2010-10-11 16:21:37 +0400

    Downgraded package version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'Snake',
-    version = '0.1dev',
+    version = '0.0.1dev',
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
