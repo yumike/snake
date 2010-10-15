49bf60c6bfc1041e771744d7b223b001a79c9683
Mike Yumatov <mike@yumatov.org>
2010-10-15 23:47:13 +0400

    Temporarily added info output to sh function.

--- snake/utils.py
+++ snake/utils.py
@@ -5,4 +5,5 @@ def sh(command, capture=False):
     kwargs = {"shell": True}
     if capture:
         kwargs['stdout'] = subprocess.PIPE
+    print("[sh] %s" % command)
     return subprocess.Popen([command], **kwargs).communicate()[0]
