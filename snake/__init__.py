630d9aa55848be0e20161517f7702209ade88a97
Mike Yumatov <mike@yumatov.org>
2011-02-13 14:42:59 +0300

    Move utils to helpers

--- snake/__init__.py
+++ snake/__init__.py
@@ -1,5 +1,5 @@
 from .core import snake
-from .utils import depends_on, not_a_task
+from .helpers import depends_on, not_a_task
 
 
 sh = snake.sh
--- snake/helpers.py
+++ snake/helpers.py
@@ -1,10 +1,10 @@
-def not_a_task(f):
-    f.not_a_task = True
-    return f
-
-
 def depends_on(*tasks):
     def decorator(f):
         f.depends_on = tasks
         return f
     return decorator
+
+
+def not_a_task(f):
+    f.not_a_task = True
+    return f
