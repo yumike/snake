eec83a4e23e7408dac8edb946cbee4d7b47d91a7
Mike Yumatov <mike@yumatov.org>
2011-02-13 18:43:26 +0300

    Remove old tests

--- tests/test_snake.py
+++
@@ -1,9 +0,0 @@
-from snake.api import *
-from snake.namespace import Namespace
-
-
-def test_snake():
-    snake = Snake()
-    assert isinstance(snake.tasks, Namespace)
-    assert snake.tasks.name == ''
-    assert snake.tasks is snake.current_namespace
