be3f6f2a0d60b38479f18de6493d2214f72c8bae
Mike Yumatov <mike@yumatov.org>
2011-02-23 11:12:32 +0300

    Fix snakefile

--- snakefile.py
+++ snakefile.py
@@ -1,35 +1,33 @@
 from snake.core import Snake
-from snake.helpers import depends_on, task
 
 
 snake = Snake(__name__)
-sh = snake.sh
 
 
-@task
+@snake.task
 def clean():
     """remove pyc's and pyo's"""
-    sh('find . -name "*.py[co]" -delete')
+    snake.sh('find . -name "*.py[co]" -delete')
 
 
-@task
+@snake.task
 @depends_on('virtualenv.create')
 def develop():
     """create virtualenv and setup Snake into it for development"""
-    sh('bin/pip install -e .')
+    snake.sh('bin/pip install -e .')
 
 
 class Virtualenv:
 
-    @task
+    @snake.task
     def create(self):
         """create virtual environment"""
-        sh('virtualenv --no-site-packages --distribute .')
+        snake.sh('virtualenv --no-site-packages --distribute .')
 
-    @task
+    @snake.task
     def destroy(self):
         """destroy virtual environment"""
-        sh('rm -rf bin include lib src')
+        snake.sh('rm -rf bin include lib src')
 
 
 if __name__ == '__main__':
