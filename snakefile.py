fac0448b2a713613ee5bc662f55c983fc87bb17b
Mike Yumatov <mike@yumatov.org>
2011-02-23 21:53:29 +0300

    Remove unusable tasks from project snakefile

--- snakefile.py
+++ snakefile.py
@@ -1,33 +1,7 @@
-from snake import App, Snake
+from snake import *
 
 
-snake = Snake()
-
-
-@snake.task
+@task
 def clean():
     """remove pyc's and pyo's"""
-    snake.sh('find . -name "*.py[co]" -delete')
-
-
-@snake.task
-@snake.depends_on('virtualenv.create')
-def develop():
-    """create virtualenv and setup Snake into it for development"""
-    snake.sh('bin/pip install -e .')
-
-
-with snake.namespace('virtualenv'):
-    @snake.task
-    def create():
-        """create virtual environment"""
-        snake.sh('virtualenv --no-site-packages --distribute .')
-
-    @snake.task
-    def destroy():
-        """destroy virtual environment"""
-        snake.sh('rm -rf bin include lib src')
-
-
-if __name__ == '__main__':
-    App(__name__).run()
+    sh('find . -name "*.py[co]" -delete')
