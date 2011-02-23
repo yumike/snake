c13cbdbc969c324a3d2f030e0a2e04e346c84522
Mike Yumatov <mike@yumatov.org>
2011-02-23 21:52:39 +0300

    Specify what to import from snake if imported all

--- snake/__init__.py
+++ snake/__init__.py
@@ -2,6 +2,9 @@ from .app import App
 from .core import Snake
 
 
+__all__ = ['snake', 'depends_on', 'namespace', 'sh', 'task']
+
+
 snake = Snake()
 depends_on = snake.depends_on
 namespace = snake.namespace
